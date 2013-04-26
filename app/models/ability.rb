bc227bd37b583529c6afe2076fc84061905b47d9
Svilen Vassilev <svilen@rubystudio.net>
2013-04-26 13:33:45 +0300

    roles 2

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,7 +1,7 @@
 class Ability
   include CanCan::Ability
   def initialize(user)
-    can :manage, :all                   # allow everyone to read everything
+    can :read, :all                   # allow everyone to read everything
     if user #&& user.admin?
       can :access, :rails_admin       # only allow admin users to access Rails Admin
       can :dashboard  
